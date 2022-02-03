b8c7345ca4d7d2ab4dcde59199efe54ce4d89a9d
jamal <you@example.com>
2022-02-03 16:46:48 +0700

    perbaiki component Skiils

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,66 +1,69 @@
-import React, { useEffect, useRef, useState } from 'react';
-import NavMobile from './utils/NavMobile';
+import React, { useEffect, useRef, useState } from "react";
+// import NavMobile from './utils/NavMobile';
 
 export default function Navbar() {
   const navbar = useRef();
   const navMobile = useRef();
   const [scrollY, setScrollY] = useState(0);
-  const [themeS, setThemeS] = useState('fa-sun');
+  const [themeS, setThemeS] = useState("fa-sun");
 
   useEffect(() => {
-    window.addEventListener('scroll', function () {
+    const theme = localStorage.getItem("theme");
+    document.querySelector("html").classList.add(theme);
+    setThemeS(theme === "dark" ? "fa-moon" : "fa-sun");
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", function () {
       setScrollY(parseInt(window.scrollY));
       const navbarHeight = navbar.current.clientHeight;
-      navbar.current.classList.toggle('bg-blue-600', scrollY > navbarHeight);
-      navbar.current.classList.toggle('dark:bg-black-09', scrollY > navbarHeight);
+      navbar.current.classList.toggle("bg-blue-600", scrollY > navbarHeight);
+      navbar.current.classList.toggle(
+        "dark:bg-black-09",
+        scrollY > navbarHeight
+      );
     });
   }, [scrollY]);
 
   const defaultTheme = () => {
-    const html = document.querySelector('html');
-    if (html.classList.value === 'light') {
-      return 'dark';
-    } else if (html.classList.value === 'dark') {
-      return 'light';
+    const html = document.querySelector("html");
+    if (html.classList.value === "light") {
+      return "dark";
+    } else if (html.classList.value === "dark") {
+      return "light";
     } else {
-      return 'dark';
+      return "dark";
     }
   };
 
-  const handleTheme = (value) => {
-    localStorage.setItem('theme', value);
-    const theme = localStorage.getItem('theme');
-    const html = document.querySelector('html');
+  const handleTheme = value => {
+    localStorage.setItem("theme", value);
+    const theme = localStorage.getItem("theme");
+    const html = document.querySelector("html");
     html.classList.add(theme);
-    if (theme === 'light') {
-      html.classList.remove('dark');
-      html.classList.remove('null');
+    if (theme === "light") {
+      html.classList.remove("dark");
+      html.classList.remove("null");
     } else {
-      html.classList.remove('light');
-      html.classList.remove('null');
+      html.classList.remove("light");
+      html.classList.remove("null");
     }
-    setThemeS(theme === 'dark' ? 'fa-moon' : 'fa-sun');
+    setThemeS(theme === "dark" ? "fa-moon" : "fa-sun");
   };
 
-  useEffect(() => {
-    const theme = localStorage.getItem('theme');
-    document.querySelector('html').classList.add(theme);
-    setThemeS(theme === 'dark' ? 'fa-moon' : 'fa-sun');
-  }, []);
-
-  const mobileMenu = (e) => {
-    if (e.target.classList.contains('fa-bars')) {
-      e.target.classList.remove('fa-bars');
-      e.target.classList.add('fa-times');
+  const mobileMenu = e => {
+    if (e.target.classList.contains("fa-bars")) {
+      e.target.classList.remove("fa-bars");
+      e.target.classList.add("fa-times");
       // navMobile.current.classList.add('active');
-      navMobile.current.style.transform = 'translateY(0)';
-      navMobile.current.style.display = 'flex';
+      navMobile.current.style.transform = "translateY(0)";
+      navMobile.current.style.display = "flex";
     } else {
-      e.target.classList.add('fa-bars');
-      e.target.classList.remove('fa-times');
+      e.target.classList.add("fa-bars");
+      e.target.classList.remove("fa-times");
       // navMobile.current.classList.remove('active');
-      navMobile.current.style.transform = 'translateY(-310px)';
-      navMobile.current.style.display = 'none';
+      navMobile.current.style.transform = "translateY(-310px)";
+      navMobile.current.style.display = "none";
     }
   };
 
@@ -69,7 +72,10 @@ export default function Navbar() {
       {/* Navbar */}
       <div className="container mx-auto flex justify-between items-center py-2">
         {/* Logo */}
-        <h3 className="font-u-mono text-3xl font-semibold text-white tracking-wide">JAMAL</h3>
+        <h3 className="font-u-mono text-3xl font-semibold text-white tracking-wide">
+          JAMAL
+        </h3>
+
         {/* Nav */}
         <ul className="nav-des text-xl font-semibold font-u-mono text-white flex cursor-pointer tracking-wide">
           <li className="ml-10" onClick={() => handleTheme(defaultTheme())}>
@@ -91,8 +97,12 @@ export default function Navbar() {
             <a href="#contact">Contact</a>
           </li>
         </ul>
+
         {/* ham menu */}
-        <ul className="ham-menu text-3xl font-semibold font-u-mono text-white flex cursor-pointer tracking-wide w-40" onClick={mobileMenu}>
+        <ul
+          className="ham-menu text-3xl font-semibold font-u-mono text-white flex cursor-pointer tracking-wide w-40"
+          onClick={e => mobileMenu(e)}
+        >
           <li className="ml-10" onClick={() => handleTheme(defaultTheme())}>
             <i className={`fas ${themeS}`}></i>
           </li>
@@ -100,6 +110,7 @@ export default function Navbar() {
             <i className="fas fa-bars"></i>
           </li>
         </ul>
+
         {/* nav mobile */}
       </div>
       <div ref={navMobile} className="nav-mobile bg-blue-600 dark:bg-black-09">
--- components/Skills.js
+++ components/Skills.js
@@ -1,5 +1,5 @@
-import Image from 'next/image';
-import SectionTitle from './utils/SectionTitle';
+import Image from "next/image";
+import SectionTitle from "./utils/SectionTitle";
 
 export default function Skills() {
   return (
@@ -7,36 +7,80 @@ export default function Skills() {
       <div className="container mx-auto">
         <SectionTitle title="SKILLS" />
         <div className="flex justify-center items-center h-5/6 lg:pt-6 sm:pt-10">
-          <div className="flex flex-wrap mx-auto w-8/12 justify-center h-3/6">
-            <div className="px-3 py-3">
-              <Image src="/img/skill/html.svg" alt="skill image" width={71} height={100} />;
+          <div className="flex flex-wrap mx-auto w-full lg:w-8/12 justify-center h-3/6">
+            {/* Images */}
+            <div className="px-3 py-3 cursor-pointer transition duration-300 transform scale-[0.7] hover:scale-[0.9] md:scale-100 md:hover:scale-125">
+              <Image
+                // className="cursor-pointer transition duration-200 hover:scale-110"
+                src="/img/skill/html.svg"
+                alt="skill image"
+                width={71}
+                height={100}
+              />
             </div>
-            <div className="px-3 py-3">
-              <Image src="/img/skill/css.svg" alt="skill image" width={71} height={100} />;
+            <div className="px-3 py-3 cursor-pointer transition duration-300 transform scale-[0.7] hover:scale-[0.9] md:scale-100 md:hover:scale-125">
+              <Image
+                src="/img/skill/css.svg"
+                alt="skill image"
+                width={71}
+                height={100}
+              />
             </div>
-            <div className="px-3 py-3">
-              <Image src="/img/skill/javascript.svg" alt="skill image" width={85} height={85} />;
+            <div className="px-3 py-3 cursor-pointer transition duration-300 transform scale-[0.7] hover:scale-[0.9] md:scale-100 md:hover:scale-125">
+              <Image
+                src="/img/skill/javascript.svg"
+                alt="skill image"
+                width={85}
+                height={85}
+              />
             </div>
-            <div className="px-3 py-3">
-              <Image src="/img/skill/bootstrap.svg" alt="skill image" width={85} height={85} />;
+            <div className="px-3 py-3 cursor-pointer transition duration-300 transform scale-[0.7] hover:scale-[0.9] md:scale-100 md:hover:scale-125">
+              <Image
+                src="/img/skill/bootstrap.svg"
+                alt="skill image"
+                width={85}
+                height={85}
+              />
             </div>
-            <div className="px-3 py-3">
-              <Image src="/img/skill/tailwind.svg" alt="skill image" width={133} height={80} />;
+            <div className="px-3 py-3 cursor-pointer transition duration-300 transform scale-[0.7] hover:scale-[0.9] md:scale-100 md:hover:scale-125">
+              <Image
+                src="/img/skill/tailwind.svg"
+                alt="skill image"
+                width={133}
+                height={80}
+              />
             </div>
-            <div className="px-3 py-3">
-              <Image src="/img/skill/react.svg" alt="skill image" width={95} height={85} />;
+            <div className="px-3 py-3 cursor-pointer transition duration-300 transform scale-[0.7] hover:scale-[0.9] md:scale-100 md:hover:scale-125">
+              <Image
+                src="/img/skill/react.svg"
+                alt="skill image"
+                width={95}
+                height={85}
+              />
             </div>
-            <div className="px-3 py-3">
-              <Image src="/img/skill/next.svg" alt="skill image" width={95} height={95} />;
+            <div className="px-3 py-3 cursor-pointer transition duration-300 transform scale-[0.7] hover:scale-[0.9] md:scale-100 md:hover:scale-125">
+              <Image
+                src="/img/skill/next.svg"
+                alt="skill image"
+                width={95}
+                height={95}
+              />
             </div>
-            <div className="px-3 py-3">
-              <Image src="/img/skill/figma.svg" alt="skill image" width={60} height={90} />;
+            <div className="px-3 py-3 cursor-pointer transition duration-300 transform scale-[0.7] hover:scale-[0.9] md:scale-100 md:hover:scale-125">
+              <Image
+                src="/img/skill/figma.svg"
+                alt="skill image"
+                width={60}
+                height={90}
+              />
             </div>
-            <div className="px-3 py-3">
-              <Image src="/img/skill/xd.svg" alt="skill image" width={82} height={80} />;
-            </div>
-            <div className="px-3 py-3">
-              <Image src="/img/skill/photoshop.svg" alt="skill image" width={82} height={80} />;
+            <div className="px-3 py-3 cursor-pointer transition duration-300 transform scale-[0.7] hover:scale-[0.9] md:scale-100 md:hover:scale-125">
+              <Image
+                src="/img/skill/xd.svg"
+                alt="skill image"
+                width={82}
+                height={80}
+              />
             </div>
           </div>
         </div>
